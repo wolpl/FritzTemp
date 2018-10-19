53f371b4a9b29c00594b87e4cb2b0c85981009be
wolpl <wolpl-dev@outlook.com>
2018-10-19 15:34:11 +0200

    add sorting to queries. add index on timestamp to Measurement entity

--- Android/FritzTemp/fritztempcommons/src/main/java/de/wpaul/fritztempcommons/MeasurementsDao.kt
+++ Android/FritzTemp/fritztempcommons/src/main/java/de/wpaul/fritztempcommons/MeasurementsDao.kt
@@ -8,23 +8,23 @@ import java.util.*
 
 @Dao
 interface MeasurementsDao {
-    @Query("select * from measurements")
+    @Query("select * from measurements order by timestamp")
     fun getAll(): List<Measurement>
 
-    @Query("SELECT distinct sensor from measurements")
+    @Query("SELECT distinct sensor from measurements order by sensor")
     fun getSensors(): List<String>
 
-    @Query("select * from measurements where sensor in (:sensors)")
+    @Query("select * from measurements where sensor in (:sensors) order by sensor,timestamp")
     fun getFromSensors(sensors: List<String>): List<Measurement>
 
-    @Query("select * from measurements where timestamp >= :d and sensor in (:sensors)")
+    @Query("select * from measurements where timestamp >= :d and sensor in (:sensors) order by timestamp")
     fun getAllAfterDateFromSensors(d: Date, sensors: List<String>): List<Measurement>
 
-    @Query("select * from measurements where timestamp >= :d")
+    @Query("select * from measurements where timestamp >= :d order by timestamp")
     fun getAllAfterDate(d: Date): List<Measurement>
 
 
-    @Query("select date(timestamp) as day,min(temperature)as min,max(temperature) as max,avg(temperature) as avg from measurements where timestamp>=:earliest group by date(timestamp)")
+    @Query("select date(timestamp) as day,min(temperature)as min,max(temperature) as max,avg(temperature) as avg from measurements where timestamp>=:earliest group by date(timestamp) order by date(timestamp)")
     fun getMinMaxAverageTemperatureByDaySince(earliest: Date): List<MinMaxAvgTemperatureElement>
 
     @Insert
@@ -33,9 +33,15 @@ interface MeasurementsDao {
     @Insert
     fun insert(ms: List<Measurement>)
 
+    @Query("select count(*) from measurements")
+    fun countAll(): Int
+
+    @Query("select distinct count(*) from measurements")
+    fun countAllDistinct(): Int
+
     @Query("select avg(temperature) from measurements")
     fun getAverageTemperature(): Float
 
-    @Query("delete  from measurements")
+    @Query("delete from measurements")
     fun deleteAll()
 }
